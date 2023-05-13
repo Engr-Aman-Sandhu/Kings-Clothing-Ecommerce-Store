3901b2b0d888d830e7354c0f5c3f6710c36d32d4
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-05-13 21:26:41 +0500

    Category Context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
 import './shop.styles.scss';
-import { ProductsContext } from '../../contexts/products.context';
+import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../component/product-card/product-card.component';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const {} = useContext(CategoriesContext);
   return (
     <div className="products-container">
-      {products.map((product) => (
+      {/* {products.map((product) => (
         <ProductCard key={product.id} product={product} />
-      ))}
+      ))} */}
     </div>
   );
 };
